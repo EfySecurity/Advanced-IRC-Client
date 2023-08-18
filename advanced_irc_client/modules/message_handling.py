7afd2074b0db016be99eee80e9ed0909e2d369a3
EfySecurity <142179306+EfySecurity@users.noreply.github.com>
2023-08-18 05:31:39 -0300

    message_handling.py

--- advanced_irc_client/modules/message_handling.py
+++ advanced_irc_client/modules/message_handling.py
@@ -4,9 +4,16 @@ from termcolor import colored
 from modules.connection import irc_socket
 from modules.user_actions import send_message
 
-# Funções para tratamento de mensagens, emojis, notificações, etc.
 def format_message(user, decrypted_message):
-    # Resto do código para formatação da mensagem...
+    formatted_message = f"{user}: {decrypted_message}"
+    if user == "Você":
+        formatted_message = colored(formatted_message, "blue", attrs=["bold"])
+    elif "mencionar_seu_nome" in decrypted_message.lower():
+        notify_mention(user, decrypted_message)
+        formatted_message = colored(formatted_message, "red", attrs=["bold"])
+    elif emoji.emoji_count(decrypted_message) > 0:
+        formatted_message = colored(formatted_message, "yellow")
+    return formatted_message
 
 def notify_mention(user, message):
-    # Resto do código para notificação de menção...
+    send_message(f"Você foi mencionado por {user}: {message}")
